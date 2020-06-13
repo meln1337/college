59d27673a126071957d6e2b515c13920dc90b0ec
meln1337 <53012422+meln1337@users.noreply.github.com>
2020-06-14 01:01:56 +0300

    update

--- frontend/src/redux/actions/auth.actions.js
+++ frontend/src/redux/actions/auth.actions.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const setAuthAC = payload => ({ type: 'AUTH/SET_USER', payload });
 
 export const setAuth = user => async dispatch => {
-    const response = await axios.post('http://localhost:5000/auth/login', user);
+    const response = await axios.post('/auth/login', user);
     dispatch(setAuthAC(response.data))
 }
--- frontend/src/redux/actions/zaminy.actions.js
+++ frontend/src/redux/actions/zaminy.actions.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const setData = payload => ({ type: 'ZAMINY/SET_DATA', payload });
 
 export const setDataThunk = ({ course, group }) => async dispatch => {
-    const response = await axios.get(`http://localhost:5000/api/zaminy?course=${course}&group=${group}`);
+    const response = await axios.get(`/api/zaminy?course=${course}&group=${group}`);
     dispatch(setData(response.data));
 };
 
 export const changeDataThunk = ({ id, date, day, text, course, group }) => async dispatch => {
-    await axios.put('http://localhost:5000/api/zaminy', {
+    await axios.put('/api/zaminy', {
         id, date, day, text
     });
     dispatch(setDataThunk({ course, group }))
