56961798a1660a6b1d64a80de8a9a1a977f93206
meln1337 <53012422+meln1337@users.noreply.github.com>
2020-06-18 12:03:42 +0300

    update

--- frontend/src/components/ZNO/ZNO.jsx
+++ frontend/src/components/ZNO/ZNO.jsx
@@ -5,9 +5,9 @@ export default () => {
     return (
         <div className="ZNO">
             <div className="container">
-                <h1 className="head-text">Пробне ЗНО 2020 року</h1>
+                <h1 className="head-text">ЗНО 2020 року</h1>
                 <div className="zno-padd-block padd-block">
-                    <h3 className="padd-block-head">Пробне ЗНО</h3>
+                    <h3 className="padd-block-head">ЗНО</h3>
                     <div className="video-zno">
                         <div className="video">
                             <iframe width="1280" height="627" src="https://www.youtube.com/embed/LoDWywv1iW4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
